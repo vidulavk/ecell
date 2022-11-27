fcda3be6a986f826e6e15751bb1df923ce127200
VK Vidula <1264014060@sastra.ac.in>
2022-11-27 17:40:04 +0530

    inital

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var slideindex=0;
+showfunction();
+
+function showfunction()
+{
+    var slides=document.getElementsByClassName("myslides");
+    var i;
+    for(i=0;i<slides.length;i++)
+    { 
+      slides[i].style.display="none";
+    }
+    slideindex++;
+    if(slideindex>slides.length)
+    {
+        slideindex=1;
+    }
+    slides[slideindex-1].style.display="block";
+    setTimeout(showSlides,2500);
+}
+ 
